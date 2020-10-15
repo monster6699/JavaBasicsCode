42a8037c731d0f39a96e1a0c15db1f9e0074924a
monster <1241340047@qq.com>
2020-10-15 20:09:18 +0800

    update

--- FileCode/src/demo04.java
+++ FileCode/src/demo04.java
@@ -3,18 +3,18 @@ import java.io.IOException;
 
 public class demo04 {
     public static void main(String[] args) throws IOException {
-//        File file = new File("FileCode/a.txt");
-//        System.out.println(file.createNewFile());
-//        System.out.println(file.delete());
+        File file = new File("FileCode/a.txt");
+        System.out.println(file.createNewFile());
+        System.out.println(file.delete());
 
-//
+
         File file1 = new File("FileCode/TestDir");
         System.out.println(file1.mkdir());
-        File file = new File("FileCode/TestDir/at.txt");
-        System.out.println(file.createNewFile());
+        File file2 = new File("FileCode/TestDir/at.txt");
+        System.out.println(file2.createNewFile());
 
         // 先删除目录中的文件再删除目录，否则报错
-        System.out.println(file.delete());
+        System.out.println(file2.delete());
         System.out.println(file1.delete());
 
 
