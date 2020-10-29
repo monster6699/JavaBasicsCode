06e05552f567151a55692321b04b5e5a4bb0bdf1
monster <1241340047@qq.com>
2020-10-29 22:40:42 +0800

    update

---
+++ JDKNewCharacter/src/demo01/EableIml.java
@@ -0,0 +1,8 @@
+package demo01;
+
+public class EableIml implements EableInf {
+    @Override
+    public void eat() {
+        System.out.println("吃东西");
+    }
+}
---
+++ JDKNewCharacter/src/demo01/EableMain.java
@@ -0,0 +1,24 @@
+package demo01;
+
+public class EableMain {
+    public static void main(String[] args) {
+        EableInf eableInf = new EableIml();
+        useEable(eableInf);
+
+        useEable(new EableInf() {
+            @Override
+            public void eat() {
+                System.out.println("吃苹果");
+            }
+        });
+
+        useEable(() -> {
+            System.out.println("吃橘子");
+        });
+
+    }
+
+    public static void useEable(EableInf able) {
+        able.eat();
+    }
+}
