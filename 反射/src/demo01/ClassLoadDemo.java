125ca799b1a91212036aa5065ae4120ca60a9950
monster <1241340047@qq.com>
2020-11-12 22:17:51 +0800

    update

--- 反射/src/demo01/ClassLoadDemo.java
+++ 反射/src/demo01/ClassLoadDemo.java
@@ -3,12 +3,12 @@ package demo01;
 public class ClassLoadDemo {
     public static void main(String[] args) {
         ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
-        System.out.println(systemClassLoader);
+        System.out.println(systemClassLoader); // jdk.internal.loader.ClassLoaders$AppClassLoader@3d4eac69
 
         ClassLoader parent = systemClassLoader.getParent();
-        System.out.println(parent);
+        System.out.println(parent); // jdk.internal.loader.ClassLoaders$PlatformClassLoader@41629346
 
         ClassLoader parent1 = parent.getParent();
-        System.out.println(parent1);
+        System.out.println(parent1); // null
     }
 }
--- 反射/src/demo02/reflectDemo01.java
+++ 反射/src/demo02/reflectDemo01.java
@@ -9,16 +9,14 @@ public class reflectDemo01 {
         Constructor<?> constructor = aClass.getConstructor(String.class, int.class);
         System.out.println(constructor);
         Object o = constructor.newInstance("小明", 30);
-        System.out.println(o);
+        System.out.println(o); //Student{name='小明', age=30}
 
 
         Constructor<?> declaredConstructor = aClass.getDeclaredConstructor(String.class);
         // 因为获取的是私有方法所以使用暴力破解才能使用
         declaredConstructor.setAccessible(true);
         Object o1 = declaredConstructor.newInstance("小小明");
-        System.out.println(o1);
-
-
+        System.out.println(o1); // Student{name='小小明', age=0}
 
 
     }
